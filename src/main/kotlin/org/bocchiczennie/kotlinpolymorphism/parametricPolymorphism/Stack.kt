610fdc0f5221ed134ad46730f43a7d9553d5858c
bocchiczennie <168655063+bocchiczennie@users.noreply.github.com>
2025-02-03 23:03:37 +0900

    Update Stack.kt

--- src/main/kotlin/org/bocchiczennie/kotlinpolymorphism/parametricPolymorphism/Stack.kt
+++ src/main/kotlin/org/bocchiczennie/kotlinpolymorphism/parametricPolymorphism/Stack.kt
@@ -1,4 +1,31 @@
+/**
+ * パラメトリック多相
+ * 型パラメータを利用して、異なる型のオブジェクトを扱うことができる
+ * ここでは Stack クラスが任意の型 T に対応できる
+ * */
+
 package org.bocchiczennie.kotlinpolymorphism.parametricPolymorphism
 
-class Stack {
+class Stack<T> {
+    private val elements: MutableList<T> = mutableListOf()
+
+    fun push(element: T) {
+        elements.add(element)
+    }
+
+    fun pop(): T? {
+        if (elements.isEmpty()) return null
+        return elements.removeAt(elements.size - 1)
+    }
+}
+
+
+fun execute() {
+    val integerStack = Stack<Int>()
+    integerStack.push(1)
+    integerStack.push(2)
+
+    println(integerStack.pop()) // 2
+    println(integerStack.pop()) // 1
+    println(integerStack.pop()) // null
 }
